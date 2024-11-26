79dfa96d704cc82cccd763af480e30e862ae7dbd
marloncgomez26 <marloncgomez26@gmail.com>
2024-11-26 19:59:38 +0000

    Actualizar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 st.title('Marlon Gómez')
-st. caption('Esudiante de la materia progrmación')
+st. caption('Estudiante de la materia progrmación')
+st. caption('Amante a las matematicas y a as mujeres')
